0a6ed7f61494c0a0594b12cc589c6707457925d4
abrar <abrar.alrahman@gmail.com>
2023-12-06 20:08:50 +0300

    img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import dezion from "/public/dezion.png"
 function Header() {
   return (
     <div className='heading'>
-    <img src="./dezion.png" /> 
+    <img src={dezion} /> 
     <div className='search-bar.div'>
     <ul>
     <li className='lihome'>Home</li>
